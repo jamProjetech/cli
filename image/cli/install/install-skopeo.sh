b67e5e76a85f1cfee2b507ac88ec4c5cca6090de
David Parker <parkerda@uk.ibm.com>
2023-02-16 11:32:18 +0000

    [patch] Add skopeo-login to container image

--- image/cli/install/install-skopeo.sh
+++ image/cli/install/install-skopeo.sh
@@ -3,7 +3,7 @@
 # Install skopeo & cloudctl & hostname (needed by cloudctl) & httpd-tools (needed for htpasswd cmd)
 set -e
 
-dnf install skopeo hostname httpd-tools -y
+dnf install skopeo skopeo-login hostname httpd-tools -y
 dnf clean all
 wget -q https://github.com/IBM/cloud-pak-cli/releases/download/v3.17.0/cloudctl-linux-amd64.tar.gz
 tar -xf cloudctl-linux-amd64.tar.gz
